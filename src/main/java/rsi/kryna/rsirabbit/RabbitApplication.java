b0f7b6fa911c4d8d99ad32dbd8b22a98db8b736f
Tomasz Kryński <tokr@grapeup.com>
2022-06-19 13:03:01 +0200

    2

--- src/main/java/rsi/kryna/rsirabbit/RabbitApplication.java
+++ src/main/java/rsi/kryna/rsirabbit/RabbitApplication.java
@@ -1,13 +1,22 @@
 package rsi.kryna.rsirabbit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitApplication {
+public class RabbitApplication implements CommandLineRunner {
+
+    @Autowired
+    Sender sender;
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        sender.sendMessage();
+    }
 }
